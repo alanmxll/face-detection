6634487d5ba282740d01a7cf2fedb0d8a2ad1218
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-31 13:35:11 -0300

    camera view

--- camera/views.py
+++ camera/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse, StreamingHttpResponse, request
 
-# Create your views here.
+
+from .camera import VideoCamera
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+def camera_live(request):
+    try:
+        return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
+
+
+class Camera(View):
+    template_name = 'camera.html'
+    word = 'FaceDetection'
+
+    def get(self, request):
+        context = {
+            'data': self.word
+        }
+        return render(request, self.template_name, context)
